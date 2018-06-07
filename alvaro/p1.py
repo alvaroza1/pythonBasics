b8a7ec210e14353fb7238cbac7fc7ca0f69cb863
alvaro_za1 <alvarozetina@kybel.mx>
2018-06-06 20:17:41 -0500

    Is numeric heigth

--- alvaro/p1.py
+++ alvaro/p1.py
@@ -1,5 +1,7 @@
-height = 5
-x = 0
-while x < height:
-    print(height * 'x')
-    height -= 1
+height = input("Ingrese el height: ")
+while (height.isnumeric()):
+    height = int(height)
+    while height > 0:
+        print(height * 'x')
+        height -= 1
+    height = input("Ingrese el height: ")
